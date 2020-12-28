ac637d27d7fe2d77bae0392c7f11909e0f94796e
mealthebear <mealthebear@gmail.com>
2020-12-27 19:11:04 -0800

    Initialized file set up for React frontend

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  return (
+    <div>This is my React app! Testing 1 2 3</div>
+  )
+    
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import App from './components/App.js';
+import React from 'react';
+import { render } from 'react-dom';
+
+const root = document.getElementById('root');
+
+render(<App />, root);
