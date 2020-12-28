9623aaf308faa6b749121d0224deca258ca65307
mealthebear <mealthebear@gmail.com>
2020-12-27 19:27:35 -0800

    Added basic form for inputting user information

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  return <div>This is my React app! Testing 1 2 3</div>
-    
+  return (
+    <>
+    <h1>Sign up for the Event!</h1>
+    <h3>Welcome to the Registration Form! Please enter your
+    name and age, and we'll process your request. Thank you!
+    </h3>
+    <form>
+      <label>Name</label>
+      <input type="text"></input>
+
+      <label>Age</label>
+      <input type="text"></input>
+
+      <input type="submit" value="Submit" />
+    </form>
+    </>
+  )  
 }
 
 export default App;
